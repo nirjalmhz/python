55b508cf023994d4ca7b067385238d71234e2c61
nirjalmhz <156629886+nirjalmhz@users.noreply.github.com>
2025-06-16 17:06:08 +0545

    Add files via upload
    marks calculator

---
+++ marks.py
@@ -0,0 +1,9 @@
+mark=float(input("Enter your mark:"))
+if(mark>=90 and mark<100):
+    print("Congratulations! You got A")
+elif(mark>=80 and mark<90):
+    print("Congratulations! You got B")
+elif(mark>=70 and mark<80):
+    print("Congratulations! You got B")
+else:
+    print("You are Fail")
